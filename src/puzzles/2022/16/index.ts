7ed9abb53b2bec8684471aae27740d866fd00452
Martin Doyle <github@martindoyle.com>
2022-12-17 09:28:21 +0000

    feat(puzzles): Further WIP on Day 16

--- src/puzzles/2022/16/index.ts
+++ src/puzzles/2022/16/index.ts
@@ -1,13 +1,5 @@
 import { logAnswer, readData } from '../../../utils';
 
-type MoveToAndOpenValveFn = (params: {
-  flow: number;
-  minute: number;
-  path: string[];
-  totalReleased: number;
-  valve: Valve;
-}) => void;
-
 const USE_TEST_DATA = true;
 const DATA = readData({
   importUrl: import.meta.url,
@@ -21,12 +13,13 @@ console.log({ DATA });
 
 const LINE_REGEX =
   /^Valve (?<name>[A-Z]{2}) has flow rate=(?<flowRate>\d+); tunnels? leads? to valves? (?<connectedValves>[A-Z, ]+)$/u;
-const MAX_RECENT_VALVES = 1;
 
 class Valve {
   public readonly name: string;
 
   public flowRate?: number;
+
+  public isOpen = false;
 
   private connectedTo: Valve[] = [];
 
@@ -43,9 +36,10 @@ class Valve {
   }
 }
 
-const runOne = () => {
-  // Construct graph
-  const valvesGraph = new Map<string, Valve>();
+const VALVES_MAP = (() => {
+  const valvesMap = new Map<string, Valve>();
+
+  // Loop through all lines
   for (const nextLine of DATA) {
     // Parse next line
     const match = LINE_REGEX.exec(nextLine);
@@ -53,132 +47,95 @@ const runOne = () => {
       throw new Error(`Unable to parse line: ${nextLine}`);
     }
     const { name, flowRate, connectedValves } = match.groups!;
-    // console.log({ connectedValves, flowRate, name });
 
     // Get/create next valve
-    let nextValve = valvesGraph.get(name!);
+    let nextValve = valvesMap.get(name!);
     if (!nextValve) {
       nextValve = new Valve(name!);
-      valvesGraph.set(name!, nextValve);
+      valvesMap.set(name!, nextValve);
     }
     nextValve.flowRate = Number.parseInt(flowRate!, 10);
 
     // Connect to real or placeholder valves
     for (const nextValveName of connectedValves!.split(', ')) {
-      let nextConnectedValve = valvesGraph.get(nextValveName);
+      let nextConnectedValve = valvesMap.get(nextValveName);
       if (!nextConnectedValve) {
         nextConnectedValve = new Valve(nextValveName);
-        valvesGraph.set(nextValveName, nextConnectedValve);
+        valvesMap.set(nextValveName, nextConnectedValve);
       }
       nextValve.connectTo(nextConnectedValve);
     }
   }
 
-  // console.log({ valvesGraph });
+  return valvesMap;
+})();
 
-  // Define recursive function for DFS
-  const MAX_TIME = 30;
-  const openValves: Valve[] = [];
-  let maxReleased = 0;
-  const moveToValve: MoveToAndOpenValveFn = ({ flow, minute, totalReleased, valve, path }) => {
-    // console.log(path.join('->') + '\n');
+const getShortestPath = (valve1: Valve, valve2: Valve): string[] => {
+  // const distances = new Map<string, number>();
+  // const toVisit: Valve[] = [valve1];
+  // while (toVisit.length > 0) {
+  //   const nextValve = toVisit.shift()!;
+  //   const distance = path.length;
+  //   const thisDistance = distances.get(nextValve.coords)!;
+  //   if (nextValve.isFinished) {
+  //     shortestDistance = thisDistance;
+  //     break;
+  //   }
 
-    // Setup running counters
-    let thisFlow = flow;
-    let thisTotalReleased = totalReleased;
-    let thisMinute = minute;
+  //   for (const nextNeighbour of validNeighbours) {
+  //     toVisit.push(nextNeighbour);
+  //     distances.set(nextNeighbour.coords, thisDistance + 1);
+  //   }
+  // }
 
-    // Take one minute to get here: add minute and update total released.
-    thisMinute += 1;
-    thisTotalReleased += thisFlow;
+  const shortestPath = [valve1.name, valve2.name];
 
-    //     console.log(`== Minute ${thisMinute} ==
-    // ${
-    //   openValves.length === 0
-    //     ? 'No valves are open.'
-    //     : openValves.length === 1
-    //     ? `Valve ${openValves[0]!.name} is open, releasing ${thisFlow} pressure.`
-    //     : `Valves ${openValves
-    //         .map((nextValve) => nextValve.name)
-    //         .join(', ')} are open, releasing ${thisFlow} pressure.`
-    // }`);
+  return shortestPath!;
+};
 
-    // If finished, store max and exit
-    if (thisMinute === MAX_TIME) {
-      if (thisTotalReleased > maxReleased) {
-        console.log(`New max of ${thisTotalReleased} at ${path.join('->')}`);
+const SHORTEST_PATHS = (() => {
+  const shortestPaths = new Map<string, string[]>();
+  const allValves = [...VALVES_MAP.values()];
+
+  let outerLoopCount = 0;
+  let activeLoopCount = 0;
+  for (const startValve of allValves) {
+    for (const endValve of allValves) {
+      outerLoopCount++;
+      const pathName = `${startValve.name}->${endValve.name}`;
+      // console.log(`Finding shortest path for ${pathName} ...`);
+
+      // Set empty path for self-to-self
+      if (startValve === endValve) {
+        shortestPaths.set(`${startValve.name}->${endValve.name}`, []);
+        continue;
       }
-      // console.log({ maxReleased, path: path.join('->') });
-      maxReleased = Math.max(maxReleased, thisTotalReleased);
-      return;
-    }
 
-    // openValveIfPossible({
-    //   flow: thisFlow,
-    //   minute: thisMinute,
-    //   path: [...path, valve.name],
-    //   totalReleased: thisTotalReleased,
-    //   valve,
-    // });
-
-    // Potentially open valve
-    let didOpen = false;
-    if (!openValves.includes(valve) && valve.flowRate! > 0) {
-      // Will take a minute to open
-      thisMinute += 1;
-      thisTotalReleased += thisFlow;
-
-      // console.log(`You open valve ${valve.name}.`);
-
-      // Open valve
-      openValves.push(valve);
-      thisFlow += valve.flowRate!;
-      didOpen = true;
-
-      // If finished, store max and exit
-      if (thisMinute === MAX_TIME) {
-        if (thisTotalReleased > maxReleased) {
-          console.log(`New max of ${thisTotalReleased} at ${path.join('->')}`);
-        }
-        // console.log({ maxReleased, path: path.join('->') });
-        maxReleased = Math.max(maxReleased, thisTotalReleased);
-        return;
+      // If reverse path exists, re-use it
+      const reversePathName = `${endValve.name}->${startValve.name}`;
+      const reversePath = shortestPaths.get(reversePathName);
+      if (reversePath !== undefined) {
+        const thisPath = reversePath.slice().reverse();
+        shortestPaths.set(pathName, thisPath);
+        continue;
       }
+
+      // Perform BFS to find shortest path
+      activeLoopCount++;
+      const shortestPath = getShortestPath(startValve, endValve);
+      shortestPaths.set(`${startValve.name}->${endValve.name}`, shortestPath);
     }
-
-    // Continue to next valves
-    for (const nextValve of valve.connectedValves.filter((nextConnectedValve) => {
-      // Don't go back to valves unless we've opened a valve this time
-      // console.log({ lastValves: path.slice(-MAX_RECENT_VALVES).join('->'), path: path.join('->') });
-      const isTooRecent = path.slice(-MAX_RECENT_VALVES).includes(nextConnectedValve.name);
-      const isValid = didOpen || !isTooRecent;
-      return isValid;
-    })) {
-      moveToValve({
-        flow: thisFlow,
-        minute: thisMinute,
-        path: [...path, valve.name],
-        totalReleased: thisTotalReleased,
-        valve: nextValve,
-      });
-    }
-
-    // console.log({ currentFlow: flow, minute, totalReleased, valve });
-  };
-
-  // Start DFS to find highest flow
-  const startingNode = valvesGraph.get('AA')!;
-  for (const nextValve of startingNode.connectedValves) {
-    moveToValve({
-      flow: 0,
-      minute: 0,
-      path: ['AA'],
-      totalReleased: 0,
-      valve: nextValve,
-    });
   }
 
-  logAnswer(1, maxReleased);
+  console.log({ activeLoopCount, outerLoopCount });
+
+  return shortestPaths;
+})();
+
+console.log(JSON.stringify(SHORTEST_PATHS));
+
+const runOne = () => {
+  logAnswer(1, undefined);
 };
 
 const runTwo = () => {
